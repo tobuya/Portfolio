0da985940834418f2ff56b3ab5c3e0e59abb3ec2
somarib <91262542+somarib@users.noreply.github.com>
2022-11-22 17:26:19 +0000

    Add and append close icon to mobile menu

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const navigation = document.querySelector('.toolbar');
 
 const mobileMenu = document.createElement('div');
 mobileMenu.classList.add('mobile-menu');
+
+const cross = document.createElement('a');
+cross.classList.add('cross-icon');
+const crossImg = document.createElement('img');
+crossImg.src = 'images/icon-cross.svg';
+crossImg.alt = 'Close Icon';
+cross.appendChild(crossImg);
+mobileMenu.appendChild(cross);
