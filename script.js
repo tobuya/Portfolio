5806ed77bb3f065249bbe79d011c6d2dd311f467
tobuya <thomasobuya95@yahoo.com>
2022-11-22 20:29:28 +0300

    Add the mobile navigation items

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ crossImg.src = 'images/icon-cross.svg';
 crossImg.alt = 'Close Icon';
 cross.appendChild(crossImg);
 mobileMenu.appendChild(cross);
+
+const ul = document.createElement('ul');
+ul.id = 'ul';
+mobileMenu.appendChild(ul);
+
+const portfolio = document.createElement('li');
+ul.appendChild(portfolio);
+portfolio.classList.add('menu-container');
+const portfolioLink = document.createElement('a');
+portfolio.appendChild(portfolioLink);
+portfolioLink.textContent = 'Portfolio';
+portfolioLink.href = '#portfolio';
+portfolioLink.classList.add('menu-text');
