f10cdc630d391cdd94c0e75b8496483a6c16e229
tobuya <thomasobuya95@yahoo.com>
2022-11-22 20:17:36 +0300

    Add the script file and mobile menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuSection = document.querySelector('#header-section');
+const menuAnchor = document.querySelector('.menu');
+const navigation = document.querySelector('.toolbar');
+
+const mobileMenu = document.createElement('div');
+mobileMenu.classList.add('mobile-menu');
