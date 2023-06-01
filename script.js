91b0a7874183576badfa4b2e96afc7c6a5d65088
tobuya <thomasobuya95@gmail.com>
2023-06-01 21:20:07 +0300

    Update projects information

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ contactLink.addEventListener('click', displayPortfolioSection);
 cross.addEventListener('click', displayPortfolioSection);
 
 window.addEventListener('load', () => {
-  multiPostSection.innerHTML = `   
+  multiPostSection.innerHTML = `
   <h2 class="block-h2">Multi-Post Stories</h2>
   <p class="block-p">A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.</p>
   <div class="block-nav">
@@ -115,12 +115,12 @@ window.addEventListener('load', () => {
 // popup
 const projects = [
   {
-    projectName: 'Multi-Post Stories',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    projectName: 'E-Learning Academy',
+    description: 'E-learning platform where users can reserve online classes to schedule study sessions with learners in their area',
     featuredImage: 'stories.svg',
-    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    technologies: ['React', 'Redux', 'Bootstrap', 'Ruby on Rails', 'PostgreSQL'],
     live: 'https://tobuya.github.io/Portfolio/',
-    source: 'https://github.com/tobuya/Portfolio',
+    source: 'https://github.com/tobuya/e-learning-academy',
     seeLiveText: 'See Live',
     seeLiveImg: 'see-live.svg',
     seeSourceText: 'See Source',
@@ -128,20 +128,52 @@ const projects = [
     closeIcon: 'icon-cross.svg',
   },
   {
-    projectName: 'Profesional Art <br> Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    projectName: 'Kitchen Genie',
+    description: 'Kitchen Genie is a recipe sharing web app that helps home cooks find inspiration and get organized in the kitchen.',
     featuredImage: 'cardwork.svg',
-    technologies: ['html', 'bootstrap', 'Ruby'],
+    technologies: ['Ruby on Rails', 'CSS', 'PostgreSQL'],
     live: 'https://tobuya.github.io/Portfolio/',
-    source: 'https://github.com/tobuya/Portfolio',
+    source: 'https://github.com/tobuya/kitchen-genie',
   },
   {
-    projectName: 'Keeping track of hundreds  of components website',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    projectName: 'Space Travelers Hub',
+    description: 'This application is based on real life data from SpaceX API. It allows users to book a rocket and join a selected mission.',
     featuredImage: ['desktop-snap.svg', 'bg-one.svg'],
-    technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+    technologies: ['React', 'Redux', 'React Bootstrap'],
     live: 'https://tobuya.github.io/Portfolio/',
-    source: 'https://github.com/tobuya/Portfolio',
+    source: 'https://github.com/tobuya/space_travelers_hub',
+  },
+  {
+    projectName: 'Budget Tracker',
+    description: 'This application help users manage their budgets, users have a list transactions associated with a category so that they can see how much money they have spent and on what.',
+    featuredImage: 'cardwork.svg',
+    technologies: ['Ruby on Rails', 'PostgreSQL', 'CSS'],
+    live: 'https://tobuya.github.io/Portfolio/',
+    source: 'https://github.com/tobuya/budget_tracker',
+  },
+  {
+    projectName: 'Post Streams',
+    description: 'This application show users a list of posts and empower them to interact with them by adding comments and liking posts',
+    featuredImage: 'cardwork.svg',
+    technologies: ['Ruby on Rails', 'PostgreSQL', 'CSS'],
+    live: 'https://tobuya.github.io/Portfolio/',
+    source: 'https://github.com/tobuya/post_stream',
+  },
+  {
+    projectName: 'Soccer Hub',
+    description: 'This application uses ScoreBat API to allow users to watch recent official highlights of the Premier League, Champions League, La Liga, Serie A, Europa League and many more matches of your favorite football leagues.',
+    featuredImage: 'cardwork.svg',
+    technologies: ['React', 'Redux', 'CSS'],
+    live: 'https://tobuya.github.io/Portfolio/',
+    source: 'https://github.com/tobuya/kitchen-genie',
+  },
+  {
+    projectName: 'Data Science <br> Exhibition',
+    description: 'This is a Capstone project about AI and Big Data Exhibition. Users can interact with the app and test its features.',
+    featuredImage: 'cardwork.svg',
+    technologies: ['JavaScript', 'CSS', 'HTML'],
+    live: 'https://tobuya.github.io/Portfolio/',
+    source: 'https://github.com/tobuya/Data-Science-Exhibition',
   },
 ];
 
